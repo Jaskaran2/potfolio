2d07ab1f468688334b3289d8036a69f8a97e54b8
Jaskaran2 <jaskaran@kiprosh.com>
2023-10-14 23:46:36 +0530

    Add test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+    remaining essentially unchanged. It was popularised in the 1960s."
+  )
+end
+
+5.times do |skill|
+  Skill.create(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+9.times do |portfolio|
+  Portfollio.create(
+    title: "Portfolio title #{portfolio}",
+    subtitle: "My services",
+    body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+    remaining essen.",
+    main_image:"https://placehold.co/600x400.png",
+    thumb_image:"https://placehold.co/350x200.png",
+  )
+end
